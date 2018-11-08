9aa90d9a20ba79db49d86b627847d8e95c1454f0
ACoolZero <nphicuong.is@gmail.com>
2018-11-08 13:29:50 +0700

    test pgp

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,22 @@ Route::get('sharewith','HomeController@sharewith');
 Route::get('settings','HomeController@settings');
 Route::get('profiles','HomeController@profiles');
 
+Route::get('test_gpg', function () {
+    echo '<pre>';
+
+    // Outputs all the result of shellcommand "ls", and returns
+    // the last output line into $last_line. Stores the return value
+    // of the shell command in $retval.
+    $last_line = system('gpg --gen-key', $retval);
+
+    // Printing additional info
+    echo '
+    </pre>
+    <hr />Last line of the output: ' . $last_line . '
+    <hr />Return value: ' . $retval;
+
+});
+
 Route::get('init_roles', function () {
     App\Roles::create(
         [
