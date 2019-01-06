fd7fb7ff14f03e8718bb56f60fe0770b94e1392f
ACoolZero <nphicuong.is@gmail.com>
2019-01-06 16:21:04 +0700

    edit addon

--- Firefox/background.js
+++ Firefox/background.js
@@ -1,7 +1,7 @@
 var checkPGP = function (){
   browser.storage.local.get('user_pgp', function(data){
     if (!data.user_pgp){
-      console.info("PGP not found. Try to get user attention...");
+      console.info("Không có dữ liệu PGP của người dùng. Bạn cần đăng ký hoặc cài lại khoá để có thể sử dụng.");
       browser.browserAction.setTitle({title:"SecPASS - Không tìm thấy dữ liệu người dùng"});
       browser.browserAction.setIcon({path:"icon-red.png"});
     }
